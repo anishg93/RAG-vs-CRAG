3889a767b8bde70f8c981fe270bb2c68259b092d
anishg93 <anish.analytics1@gmail.com>
2024-07-28 15:07:17 +0530

    working commit

--- rag/app/main.py
+++ rag/app/main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
 from langchain.embeddings import HuggingFaceEmbeddings
 from utils import *
 from prompt_template import *
